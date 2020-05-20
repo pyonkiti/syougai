72a573d8e668504b1672e553a7a51aa497ef2009
Tanaka <pyontana@gmail.com>
2020-05-20 20:53:52 +0900

    apprication

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,9 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require bootstrap
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require moment
 //= require bootstrap-datetimepicker
 //= require_tree .
