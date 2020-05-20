8bca3caf1e04207f9be86f1fa41c1bf85dff6e94
Tanaka <pyontana@gmail.com>
2020-05-20 14:09:05 +0900

    application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,12 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require bootstrap
 //= require moment
 //= require bootstrap-datetimepicker
-//= require jquery
-//= require jquery_ujs
 //= require_tree .
