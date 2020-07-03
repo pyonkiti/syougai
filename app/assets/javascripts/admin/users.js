ef6a82dc5412357534f00eb2432f903de90712e2
Tanaka <pyontana@gmail.com>
2020-07-03 16:03:48 +0900

    パスワード表示

--- app/assets/javascripts/admin/users.js
+++ app/assets/javascripts/admin/users.js
@@ -1,14 +1,14 @@
 // ログインユーザー（編集）
 
 // パスワード表示
-$(document).on('turbolinks:load', function() {
-    $(function(){
-        $('#users_passcheck').change(function(){
-            if ( $(this).prop('checked') ) {
-                $('#users_password').attr('type','text');
-            } else {
-                $('#users_password').attr('type','password');
-            }
-        })
+// $(document).on('turbolinks:load', function() {
+$(function(){
+    $('#users_passcheck').change(function(){
+        if ( $(this).prop('checked') ) {
+            $('#users_password').attr('type','text');
+        } else {
+            $('#users_password').attr('type','password');
+        }
     })
 })
+// })
